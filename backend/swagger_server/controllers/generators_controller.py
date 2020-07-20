eebde33724ea93a7712ab703692bf033c8d1c5db
Ludwik Ciechański <luigi.zielone@gmail.com>
2020-07-20 13:43:09 +0200

    Add graph generators logic - list available, execute chosen and return data

--- backend/swagger_server/controllers/generators_controller.py
+++ backend/swagger_server/controllers/generators_controller.py
@@ -2,37 +2,81 @@ import connexion
 import six
 import networkx as nx
 
-from swagger_server.models.generator import Generator  # noqa: E501
-from swagger_server.models.graph import Graph  # noqa: E501
+from swagger_server.models.generator import Generator
+from swagger_server.models.graph import Graph
 from swagger_server import util
 
+N = 5
+# Classic
+def binomial_tree():
+    return nx.binomial_tree(N)
+def complete_graph():
+    return nx.complete_graph(N)
+def ladder_graph():
+    return nx.ladder_graph(N)
+def star_graph():
+    return nx.star_graph(N)
+def wheel_graph():
+    return nx.wheel_graph(N)
 
-def generate(body):  # noqa: E501
+# Small
+def bull_graph():
+    return nx.bull_graph()
+def diamond_graph():
+    return nx.diamond_graph()
+def frucht_graph():
+    return nx.frucht_graph()
+def icosahedral_graph():
+    return nx.icosahedral_graph()
+def krackhardt_kite_graph():
+    return nx.krackhardt_kite_graph()
+
+# Social networks
+def karate_club_graph():
+    return nx.karate_club_graph()
+def davis_southern_women_graph():
+    return nx.davis_southern_women_graph()
+def florentine_families_graph():
+    return nx.florentine_families_graph()
+def les_miserables_graph():
+    return nx.les_miserables_graph()
+
+generators_dict = {
+    "binomial_tree": binomial_tree,
+    "complete_graph": complete_graph,
+    "ladder_graph": ladder_graph,
+    "star_graph": star_graph,
+    "wheel_graph": wheel_graph,
+
+    "bull_graph": bull_graph,
+    "diamond_graph": diamond_graph,
+    "frucht_graph": frucht_graph,
+    "icosahedral_graph": icosahedral_graph,
+    "krackhardt_kite_graph": krackhardt_kite_graph,
+
+    "karate_club_graph": karate_club_graph,
+    "davis_southern_women_graph": davis_southern_women_graph,
+    "florentine_families_graph": florentine_families_graph,
+    "les_miserables_graph": les_miserables_graph,
+}
+
+def generate(body):
     """Generate certain graph
-
-     # noqa: E501
-
     :param body: Generator name (and its parameters)
     :type body: dict | bytes
-
     :rtype: Graph
     """
-    # if connexion.request.is_json:
-    #     body = Generator.from_dict(connexion.request.get_json())  # noqa: E501
-    # return 'do some magic!'
+    if not connexion.request.is_json:
+        return 'Request has to be in JSON format'
 
-    K_3_5 = nx.complete_bipartite_graph(3, 5)
-    graph_data = nx.readwrite.json_graph.cytoscape_data(K_3_5)
-
+    body = Generator.from_dict(connexion.request.get_json())
+    G = generators_dict.get(body.name, lambda: 'Invalid generator name')()
+    graph_data = nx.readwrite.json_graph.cytoscape_data(G)
     return graph_data
 
 
 def get_generators():  # noqa: E501
     """Returns supported NetowrkX generators
-
-     # noqa: E501
-
-
     :rtype: List[Generator]
     """
-    return 'do some magic!'
+    return list(generators_dict.keys())
