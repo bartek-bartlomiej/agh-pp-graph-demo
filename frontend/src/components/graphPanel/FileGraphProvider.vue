8b002b29fb790db2c7e6b4e729c581518806d16f
Bartłomiej Stępień <bartek.bartlomiej.96@gmail.com>
2020-07-21 08:37:07 +0200

    Add loading graph from file

--- frontend/src/components/graphPanel/FileGraphProvider.vue
+++ frontend/src/components/graphPanel/FileGraphProvider.vue
@@ -1,21 +1,91 @@
 <template>
-  <div></div>
+  <aside class="menu">
+    <p class="menu-label">
+      JSON / Edges List
+    </p>
+    <b-field class="has-addons" position="is-centered">
+      <b-field class="file">
+        <b-upload
+          native
+          v-model="file"
+          accept=".json,.JSON,.edges">
+          <a class="button is-primary">
+            <span>Click to upload</span>
+          </a>
+        </b-upload>
+      </b-field>
+    </b-field>
+  </aside>
 </template>
 
 <script>
-import elements from '../../assets/elements.json'
+import apiOperationMixin from '../../mixin/apiOperationMixin'
+
+function readFile (file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.onload = () => {
+      resolve(reader.result)
+    }
+    reader.onerror = () => {
+      reject(reader.error)
+    }
+    reader.readAsText(file)
+  })
+}
+
+const mixinData = {
+  operationName: 'uploadFile',
+  shouldRetry: false,
+  consoleErrorMessage: 'Could not upload and parse file',
+  toastErrorMessage: 'Could not parse file'
+}
 
 export default {
   name: 'FileGraphProvider',
-  mounted () {
-    const graph = {
-      data: {
-        name: 'elements.json',
-        provider: 'file'
-      },
-      elements
+  mixins: [
+    apiOperationMixin
+  ],
+  data () {
+    return {
+      file: undefined,
+      ...mixinData
     }
-    this.$emit('input', graph)
+  },
+  computed: {
+    operationData () {
+      const formData = new FormData()
+      formData.append('file', this.file)
+      return formData
+    }
+  },
+  watch: {
+    file (value) {
+      readFile(value)
+        .then((content) => {
+          try {
+            const elements = JSON.parse(content)
+            const graph = { elements }
+            this.passGraph(graph)
+          } catch (e) {
+            this.performOperation()
+          }
+        })
+        .catch((error) => console.error(error))
+    }
+  },
+  methods: {
+    handleOperationSucceeded (graph) {
+      this.passGraph(graph)
+    },
+    passGraph (graph) {
+      graph.data = {
+        provider: 'file',
+        name: this.file.name,
+        ...graph.data
+      }
+      this.$emit('input', graph)
+    }
   }
 }
 </script>
